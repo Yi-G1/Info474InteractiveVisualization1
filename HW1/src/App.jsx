b3ef1001800ee1f563f309076a2ddbddc468c2f8
Yi-G1 <yguo17@uw.edu>
2022-02-07 13:24:45 -0800

    added more comment

--- HW1/src/App.jsx
+++ HW1/src/App.jsx
@@ -6,22 +6,29 @@ import { AxisLeft, AxisBottom } from "@visx/axis";
 import { uniq } from "lodash";
 import { useState } from 'react'
 import logo from './logo.svg'
+import getgwg from "./gender_wage_gap.js";
 import census from "./census.js";
 import "./style.css";
 import './App.css'
 
 // var value = ArrName['key_1']; //<- ArrName is the name of your array
-const agee = []
-for (let i = 1; i < 19; i++) {
-  agee.push(5*i)
-}
-console.log(agee)
+let gwg= getgwg();
+
+console.log("gwg", gwg);
 let sex1 = census.filter(item => {return item.Sex === 1})
 let sex2 = census.filter(item => {return item.Sex === 2})
 let sex11900 = sex1.filter(item => {return item.Year === 1900})
 let sex21900 = sex2.filter(item => {return item.Year === 1900})
 let sex12000  = sex1.filter(item => {return item.Year === 2000})
 let sex22000  = sex2.filter(item => {return item.Year === 2000})
+
+// data wrangling for graph 3
+let usdata = gwg.filter(item => {return item.COU === 'USA'})
+let ustop10 = usdata.filter(item => {return item.IND === 'EMP9_9'})
+let usbot10 = usdata.filter(item => {return item.IND === 'EMP9_1'})
+let usmid = usdata.filter(item => {return item.IND === 'EMP9_5'})
+console.log("usData",usdata);
+console.log("usmid",usmid);
 
 const compData = {
   name: "2000sex1",
@@ -34,7 +41,6 @@ const compData = {
 //   color: "#ffffff",
 //   items: sex11900.map((item) => ({ ...item, value: item.People }))
 // };
-console.log("compData",compData);
 
 const sex21900Data = {
   name: "1900sex2",
@@ -102,10 +108,38 @@ There are many standards people use to measure bias, and some of them may not be
          </p>
          <br/>
 
-         <p>Firstly, let us study a dataset contains information around the world</p>
+         <p>Firstly, let us study a dataset contains information around the world. Note that the gender wage gap data is obtained from the data source and will be verified by other datasets in later homework.</p>
+        <br/>
+        <br/>
+        <p>1. We first study geographically what bias look like around the world.</p>
+         <svg/>
+         <p>2. Now we want to study how gender wage gap changed in different countries by year. </p>
+        <Legend
+          data={legendData}
+          selectedItems={selectedItems}
+          onChange={onChangeSelection}
+        />
+        <MultilineChart data={chartData} dimensions={dimensions} />
+        <p>The background is selected as gray to emphasis the text and the graph. <br/>The text is selected as white to show contrast. <br/>
+        The range for x axis and y axis are selected to cover all ranges. <br/>And only 4 grid line for y axis is chosen for clearity </p>
 
+        <p>3. Let us take a deeper look into the US--change of gender wage on top and on bottom over time. </p>
         <svg/>
 
+        <p>4. The average gender wage difference by countries.  The county with most extreme bias towards male and female are: </p>
+        <svg/>
+
+        <p>5.The average gender wage difference by year -- have we get better each year</p>
+        <svg/>
+
+        <p>6. In 2002, the percentage of countries bias towards women/man.</p>
+        <svg/>
+
+        <p>7. In 2018, the percentage of countries bias towards women/man.</p>
+        <svg/>
+
+        <p>8. In 2018, the percentage of countries bias towards women/man.</p>
+        <svg/>
 
     </div>
 
